d4bfd7f98ee245ccaf963d480067ce4d2483d4f2
iamcynthiamamah <100279189+iamcynthiamamah@users.noreply.github.com>
2022-08-24 01:32:13 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for char in range(26):
-        if char != 4 and char != 16:
-                    print("{:s}".format(chr(char + ord("a"))), end="")
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print("{}".format(chr(i)), end="")
